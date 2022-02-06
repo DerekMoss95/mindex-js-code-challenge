343d230cb269a712962a9abdd8b6a9e2b2ec632f
Derek Moss <mossderek88@gmail.com>
2022-02-06 14:06:28 -0500

    Made a direct report basic table, need to add edit and delete buttons

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,6 +1,10 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { catchError, map, reduce } from 'rxjs/operators';
+import { MatTableModule } from '@angular/material/table'
+import { Employee } from '../employee';
+import { EmployeeListComponent } from '../employee-list/employee-list.component';
+import { EmployeeService } from '../employee.service';
 
-import {Employee} from '../employee';
 
 @Component({
   selector: 'app-employee',
@@ -8,8 +12,46 @@ import {Employee} from '../employee';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent {
+  compensation: number;
   @Input() employee: Employee;
+  reports = [];
+  table: any;
+  headers = ["firstName", "lastName", "position"]
+  errorMessage: string;
 
-  constructor() {
+
+
+  constructor(private employeeService: EmployeeService) {
+  }
+
+  private totalReports(ids) {
+    if (ids !== undefined) {
+      let numReports = ids.length
+      return numReports
+    } else {
+      return 0
+    }
+  }
+
+  private directReportsList() {
+    if (this.employee.directReports) {
+      this.employee.directReports.forEach(element => {
+        this.employeeService.get(element).subscribe(restItems => {
+          this.reports.push(restItems)
+        })
+      })
+    } else {
+      return
+    }
+  }
+
+
+  ngOnInit(): void {
+    this.directReportsList()
+  }
+
+  private handleError(e: Error | any): string {
+    console.error(e);
+    return this.errorMessage = e.message || 'Unable to retrieve employee data';
   }
 }
